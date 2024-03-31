c2efdf935e7ea5b078b68f56cab3616fac9b9cf8
Dhineshbabu Muthuraj <dhineshbabumuthuraj@Dhineshbabus-MacBook-Pro.local>
2024-03-31 16:25:38 -0700

    before jpa repository

---
+++ jpa/README.md
@@ -0,0 +1,13 @@
+## Some Notes
+
+<hr />
+
+* Database Class Diagram
+  * ![img.png](img.png)
+* JPA vs Hibernate vs Spring Data JPA
+  * ![img_1.png](img_1.png)
+* ![img_2.png](img_2.png)
+* ![img_3.png](img_3.png)
+* ![img_4.png](img_4.png)
+* ![img_5.png](img_5.png)
+* 
---
+++ jpa/src/main/java/com/performance/jpa/models/Author.java
@@ -0,0 +1,58 @@
+package com.performance.jpa.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+//@Table(name = "AUTHOR_TBL")
+public class Author {
+    @Id
+    /*
+    @GeneratedValue (
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )
+
+    @SequenceGenerator(
+            name = "author_seq",
+            sequenceName = "author_seq",
+            allocationSize = 1
+    )
+
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+    */
+
+    private Integer id;
+    @Column(
+            name = "f_name",
+            length = 35
+    )
+    private String firstName;
+    private String lastName;
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+    private int age;
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+    @Column (
+            insertable = false
+    )
+    private LocalDateTime lastModified;
+}
